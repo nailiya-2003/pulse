e3c241fa168a62247227333cc5769b0ba2933272
Naila <naile.gashimova@gmail.com>
2023-07-30 16:20:17 +0400

    edit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,36 @@
 
-// $(document).ready(function(){
-//       $('.carousel__inner').slick({
-//             infinite: true,
-//             speed: 1000,
-//             adaptiveHeight: true,
-//             autoplay: true,
-//             autoplaySpeed: 1500,
-//             prevArrow: '<button type="button" class="slick-prev"><img src="assets/images/previous.png" alt="previous"></button>',
-//             nextArrow: '<button type="button" class="slick-next"><img src="assets/images/next.png" alt="next"></button>',
-//             responsive: [
-//                   {
-//                   breakpoint: 996,
-//                   settings: {
-//                         arrow: false,
-//                         dots: true
-//                   }
-//                   }
-//             ]
-//       }     
-//       );
+$(document).ready(function(){
+      $('.carousel__inner').slick({
+            infinite: true,
+            speed: 1000,
+            adaptiveHeight: true,
+            autoplay: true,
+            autoplaySpeed: 1500,
+            prevArrow: '<button type="button" class="slick-prev"><img src="assets/images/previous.png" alt="previous"></button>',
+            nextArrow: '<button type="button" class="slick-next"><img src="assets/images/next.png" alt="next"></button>',
+            responsive: [
+                  {
+                  breakpoint: 996,
+                  settings: {
+                        arrow: false,
+                        dots: true
+                  }
+                  }
+            ]
+      }     
+      );
+});
+// const slider = tns({
+//       container: '.carousel__inner',
+//       items: 1,
+//       slideBy: 'page',
+//       autoplay: false,
+//       controls: false,
+//       nav: false
+//     });
+// document.querySelector(".prev").addEventListener('click', function() {
+//       slider.goTo("prev")
 // });
-const slider = tns({
-      container: '.carousel__inner',
-      items: 1,
-      slideBy: 'page',
-      autoplay: false,
-      controls: false,
-      nav: false
-    });
-document.querySelector(".prev").addEventListener('click', function() {
-      slider.goTo("prev")
-});
-document.querySelector(".next").addEventListener('click', function() {
-      slider.goTo("next")
-});
+// document.querySelector(".next").addEventListener('click', function() {
+//       slider.goTo("next")
+// });
